fff890cce80f661bcd135f0db80c9daa70029836
Bhoomi Shakya <94596019+BhoomiShakya@users.noreply.github.com>
2023-09-10 16:30:51 +0530

    Initial commit

---
+++ src/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+function Footer() {
+  const d = new Date();
+  let year = d.getFullYear();
+  return (
+    <footer>
+      <p>Copyright ⓒ {year}</p>
+    </footer>
+  );
+}
+export default Footer;
